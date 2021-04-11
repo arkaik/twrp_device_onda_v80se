a8071d2d16fcaa70e4a6857b4709e6b7d46e4f7e
Fran <anselmo.lopez.c@gmail.com>
2021-04-11 23:39:17 +0200

    Changing files

--- device.mk
+++ device.mk
@@ -10,11 +10,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/kernel:kernel \
-    $(LOCAL_PATH)/recovery/root/etc/recovery.fstab:recovery.fstab \
-    $(LOCAL_PATH)/modules/modules/nand.ko:root/nand.ko \
-    $(LOCAL_PATH)/modules/modules/sunxi_tr.ko:root/sunxi_tr.ko \
-    $(LOCAL_PATH)/modules/modules/disp.ko:root/disp.ko \
-    $(LOCAL_PATH)/modules/modules/hdmi.ko:root/hdmi.ko \
-    $(LOCAL_PATH)/modules/modules/sw-device.ko:root/sw-device.ko \
-    $(LOCAL_PATH)/modules/modules/gslX680new.ko:root/gslX680new.ko \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir,root)
+    $(LOCAL_PATH)/recovery/root/etc/recovery.fstab:recovery.fstab
+   
